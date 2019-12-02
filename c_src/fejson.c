b697caf3776a9aa1efacfadfff7ce5fe3e2484ea
Ricardo Azpeitia <razpeitia@gmail.com>
2019-12-01 19:18:53 -0800

    initial commit

---
+++ c_src/fejson.c
@@ -0,0 +1,31 @@
+#include "erl_nif.h"
+
+static int
+load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+{
+    return 0;
+}
+
+static int
+upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM load_info)
+{
+    return 0;
+}
+
+static void
+unload(ErlNifEnv* env, void* priv)
+{
+    return;
+}
+
+static ERL_NIF_TERM
+hello(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return enif_make_int(env, 4);
+}
+
+static ErlNifFunc nif_funcs[] = {
+    {"hello", 0, hello}
+};
+
+ERL_NIF_INIT(fejson, nif_funcs, &load, NULL, &upgrade, &unload);
